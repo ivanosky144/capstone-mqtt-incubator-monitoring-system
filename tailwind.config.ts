d433b5c6666bec14026558cb607d5b5a7a924b8f
ivanosky144 <ivanoskytan@gmail.com>
2024-10-08 11:16:01 +0700

    fix: update panel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
       },
       colors: {
         white_purple: '#E6E0FF',
-        ultralight_purple: '#ddd7f7',
+        ultralight_purple: '#e8e3fa',
         light_purple: '#D0C7F8',
         purple: '#988BCD',
         dark_purple: '#4E3A9D'
